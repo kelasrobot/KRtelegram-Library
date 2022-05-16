b832ce4879792e4d6bd65c0b9b18ae1b6113060f
Ajang Rahmat <ajangrahmat@gmail.com>
2022-05-16 09:27:06 +0700

    Add ESP32 Support

--- src/KRtelegram.h
+++ src/KRtelegram.h
@@ -1,11 +1,27 @@
-#define USE_CLIENTSSL true
+#define USE_CLIENTSSL true  
 #include <AsyncTelegram2.h>
+// Timezone definition
 #include <time.h>
-#define MYTZ "WIB-7"
-#include <ESP8266WiFi.h>
-BearSSL::WiFiClientSecure client;
-BearSSL::Session   session;
-BearSSL::X509List  certificate(telegram_cert);
+#define MYTZ "CET-1CEST,M3.5.0,M10.5.0/3"
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>
+  BearSSL::WiFiClientSecure client;
+  BearSSL::Session   session;
+  BearSSL::X509List  certificate(telegram_cert);
+  
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <WiFiClient.h>
+  #if USE_CLIENTSSL
+    #include <SSLClient.h>  
+    #include "tg_certificate.h"
+    WiFiClient base_client;
+    SSLClient client(base_client, TAs, (size_t)TAs_NUM, A0, 1, SSLClient::SSL_ERROR);
+  #else
+    #include <WiFiClientSecure.h>
+    WiFiClientSecure client;  
+  #endif
+#endif
 AsyncTelegram2 myBot(client);
 
 void setTelegram(char* ssid, char* pass, char* token) {
@@ -18,10 +34,22 @@ void setTelegram(char* ssid, char* pass, char* token) {
     Serial.print(".");
     delay(1000);
   }
+  #ifdef ESP8266
+  // Sync time with NTP, to check properly Telegram certificate
   configTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
+  //Set certficate, session and some other base client properies
   client.setSession(&session);
   client.setTrustAnchors(&certificate);
   client.setBufferSizes(1024, 1024);
+#elif defined(ESP32)
+  // Sync time with NTP
+  configTzTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
+  #if USE_CLIENTSSL == false
+    client.setCACert(telegram_cert);
+  #endif
+#endif
+  
+  // Set the Telegram bot properies
   myBot.setUpdateTime(2000);
   myBot.setTelegramToken(token);
   Serial.print("\nCek Koneksi ke Telegram Bot... ");
